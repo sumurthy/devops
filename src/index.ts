8cdfeb06c73deebce73ba4913f346b1f68a98b12
Sudhi <Murthy@euahns-MBP.guest.corp.microsoft.com>
2019-11-25 10:56:21 -0800

    Starter

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// tslint:disable-next-line: no-implicit-dependencies
+import { hello } from './message';
+
+document.body.onload = addElement;
+
+function addElement() {
+  // create a new div element
+  const newDiv = document.createElement('div');
+  // and give it some content
+  const newContent = document.createTextNode(hello());
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById('div1');
+  document.body.insertBefore(newDiv, currentDiv);
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+};
